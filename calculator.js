44325f1bf756720ec8708777968158608762cffc
munachiso <ifechukwumunachiso5@gmail.com>
2022-06-22 00:08:42 -0700

    check invalid input

--- calculator.js
+++ calculator.js
@@ -9,33 +9,39 @@ let result;
 const number1 = parseFloat(prompt('Enter first number: '));
 const number2 = parseFloat(prompt('Enter second number: '));
 
+// checking for invalid operand .
+// if i use number1 == NaN it will always return false so the better way is 
+if (Number.isNaN(number1)|| Number.isNaN(number2) ){
+    alert('invalid input')
+} else {
+    // take the operator input
+    const operator = prompt('Enter operator ( either +, -, * or / ): ');
 
-// take the operator input
-const operator = prompt('Enter operator ( either +, -, * or / ): ');
+    // i can either use if ... else if ... else for this or switch. They pretty much react the same way.
+    switch(operator) {
+        case '+':
+            result = number1 + number2;
+            alert("your answer is " + result)
+            break;
 
-// i can either use if ... else if ... else for this or switch. They pretty much react the same way.
-switch(operator) {
-    case '+':
-         result = number1 + number2;
-         alert("your answer is " + result)
-        break;
+        case '-':
+            result = number1 - number2;
+            alert("your answer is " + result)
+            break;
 
-    case '-':
-         result = number1 - number2;
-         alert("your answer is " + result)
-        break;
+        case '*':
+            result = number1 * number2;
+            alert("your answer is " + result)
+            break;
 
-    case '*':
-         result = number1 * number2;
-         alert("your answer is " + result)
-        break;
+        case '/':
+            result = number1 / number2;
+            alert("your answer is " + result)
+            break;
 
-    case '/':
-         result = number1 / number2;
-         alert("your answer is " + result)
-        break;
+        default:
+            alert('Invalid input ');
+            break;
+    }
+    }
 
-    default:
-        alert('Invalid input ');
-        break;
-}
